de63ebd2a4f3f4292157be37b13b3103b4d9dbb0
eporada <e.porada8@gmail.com>
2023-12-30 11:17:41 +0100

    init

---
+++ Project/Models/Patient.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Project.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public int Height { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
