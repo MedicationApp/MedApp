153698e7dcd4f10c91cf99c4e8a5bcb67638ce5b
eporada <e.porada8@gmail.com>
2024-01-18 14:06:51 +0100

    added checkbox observer

--- Project/Models/Medication.cs
+++ Project/Models/Medication.cs
@@ -6,7 +6,37 @@ namespace Project.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool Selected { get; set; }
+        private bool _selected { get; set; }
+
+        public bool Selected
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+                NotifyObservers();
+            }
+        }
+
+        private List<ICheckboxObserver> observers = new List<ICheckboxObserver>();
         public MedicationInfo medInfo { get; set; }
+
+        public void AddObserver(ICheckboxObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(ICheckboxObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        private void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.OnCheckboxChanged(this);
+            }
+        }
     }
 }
--- Project/Models/Patient.cs
+++ Project/Models/Patient.cs
@@ -2,27 +2,45 @@ using System;
 
 namespace Project.Models
 {
-    public class Patient
+    public class Patient : ICheckboxObserver
     {
         public PatientInfo patientInfo { get; set; }
         public List<Medication> medList { get; set; }
 
-        public Medication GetMedication(int id){
+        public List<Medication> leftForToday { get; set; }
+
+        public Medication GetMedication(int id)
+        {
 
             return medList.Find(med => med.Id == id);
         }
 
-        public void EditMedication(Medication med){
+        public void EditMedication(Medication med)
+        {
             int index = medList.FindIndex(m => m.Id == med.Id);
             medList[index] = med;
         }
 
-        public void AddMedication(Medication med){
+        public void AddMedication(Medication med)
+        {
             medList.Add(med);
         }
 
-        public void DeleteMedication(Medication med){
+        public void DeleteMedication(Medication med)
+        {
             medList.Remove(med);
+        }
+
+        public void OnCheckboxChanged(Medication medication)
+        {
+            if (medication.Selected)
+            {
+                leftForToday.Remove(medication);
+            }
+            else
+            {
+                leftForToday.Add(medication);
+            }
         }
     }
 }
